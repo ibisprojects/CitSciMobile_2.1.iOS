d768b394d1930a2948f5dc3b158d66aee2975e5c
lee casuto <bruiselee13@gmail.com>
2014-01-16 11:11:24 -0700

    Initial Commit

---
+++ CitSciMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CitSciMobile
+//
+//  Created by lee casuto on 1/16/14.
+//  Copyright (c) 2014 lee casuto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CitSciMobileTests/CitSciMobileTests.m
@@ -0,0 +1,34 @@
+//
+//  CitSciMobileTests.m
+//  CitSciMobileTests
+//
+//  Created by lee casuto on 1/16/14.
+//  Copyright (c) 2014 lee casuto. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CitSciMobileTests : XCTestCase
+
+@end
+
+@implementation CitSciMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
